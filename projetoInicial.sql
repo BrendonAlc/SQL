264a7f7cb79da6459e9501e294453de1149deafe
BrendonAlc <brendon_rodrigo@hotmail.com>
2022-09-05 01:24:28 -0300

    Projeto incial de cursos

---
+++ projetoInicial.sql
@@ -0,0 +1,149 @@
+CREATE DATABASE projetoInicial;
+
+/*Criando tabela de Aluno*/
+CREATE TABLE aluno (
+    id SERIAL PRIMARY KEY, /*Identificador único*/
+	primeiro_nome VARCHAR(255) NOT NULL,
+	ultimo_nome VARCHAR(255) NOT NULL,
+	data_nascimento DATE NOT NULL
+);
+
+/*Criando tabela de categoria*/
+CREATE TABLE categoria (
+    id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL UNIQUE
+);
+
+/*Criando tabela de curso*/
+CREATE TABLE curso (
+    id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	categoria_id INTEGER NOT NULL REFERENCES categoria(id)
+);
+
+/*Criando tabela de aluno_curso*/
+CREATE TABLE aluno_curso (
+	aluno_id INTEGER NOT NULL REFERENCES aluno(id),
+	curso_id INTEGER NOT NULL REFERENCES curso(id),
+	PRIMARY KEY (aluno_id, curso_id)
+);
+
+/*Inserindo valores a tabela de aluno*/
+INSERT into aluno(primeiro_nome, ultimo_nome,data_nascimento) 
+values ('Vinicius', 'Dias', '1997-10-15'),('Patricia', 'Freitas', '1986-10-25'),
+('Diogo', 'Freitas', '1984-08-27'), ('Maria', 'Rosa', '1985-01-01');
+
+
+/*Inserindo valores a tabela categoria*/
+insert into categoria (nome) VALUES ('Front-end'),('Prorgramação'),('Banco de dados'),('Data Science');
+
+
+/*Inserindo valores a tabela curso*/
+INSERT into curso (nome, categoria_id) VALUES
+('HTML',1),
+('CSS',1),
+('JS',1),
+('PHP',2),
+('JAVA',2),
+('C++',2),
+('PostgreSQL',3),
+('MySQL', 3),
+('Oracle', 3),
+('SQL Server', 3),
+('SQLite',3),
+('Pandas', 4),
+('Machine Learning', 4),
+('Power BI', 4);
+
+
+/*Inserindo valores a tabela aluno_curso*/
+insert into aluno_curso values (1,4), (1,11),(2,1), (2,2), (3,4), (3,3), (4,4), (4,6), (4,5);
+
+select * from aluno_curso;
+select * from categoria;
+
+/*Alterando nome de Data Science*/
+update categoria set nome = 'Ciência de Dados' where id = 4;
+
+
+/*Quantidade de curso por aluno (poderá alterar o limit)*/
+select aluno.primeiro_nome, aluno.ultimo_nome, 
+		count (aluno_curso.curso_id) numero_cursos 
+	from aluno
+		join aluno_curso ON aluno_curso.aluno_id = aluno.id
+group by 1, 2
+order by numero_cursos DESC
+	limit 1;
+
+/*Seleção de curso com quantidade de aluno*/
+select curso.nome,
+			count(aluno_curso.aluno_id) numero_alunos
+	from curso
+		join aluno_curso ON aluno_curso.curso_id = curso.id
+group by 1
+order by numero_alunos DESC
+	limit 1;
+	
+select * from curso;
+select * from categoria;
+
+/*Selecionando com IN*/
+select * from curso where categoria_id in (1,2);
+select id FROM categoria where nome not like '% %';
+
+/*Querie Aninhadas*/
+select curso.nome from curso where categoria_id IN (
+	select id from categoria where nome like '% de %');
+	
+
+/*Select buscando quantidade de cursos e quantas categorias existentes*/
+
+select *
+	FROM aluno
+	JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+	JOIN curso ON curso.id = aluno_curso.curso_id;
+
+select categoria.nome AS categoria,
+		Count(curso.id) AS numero_cursos
+	FROM categoria
+	JOIN curso ON curso.categoria_id = categoria.id
+GROUP BY categoria;
+
+	
+select categoria
+		FROM (
+				select categoria.nome AS categoria,
+						Count(curso.id) AS numero_cursos
+					FROM categoria
+					JOIN curso ON curso.categoria_id = categoria.id
+			GROUP BY categoria
+		) AS categoria_cursos
+	WHERE numero_cursos >= 3;
+	
+select * from categoria;
+
+select curso.nome,
+		count(aluno_curso.aluno_id) numero_alunos
+	from curso
+	join aluno_curso ON aluno_curso.curso_id = curso.id
+group by 1
+	having count(aluno_curso.aluno_id) > 2
+order by numero_alunos DESC;
+
+/*Corrigindo nome do curso*/
+update categoria set nome = 'Programação' where id = 2;
+
+
+/*Funcoes, concatenacao*/
+select (primeiro_nome || ' ' || ultimo_nome) as nome_completo from aluno;
+
+select concat('Vinicius', ' ', 'Dias');
+
+/*UPPER (caixa alta) / TRIM (excluir os espacos do meio e fim)*/
+select UPPER(concat('Vinicius', ' ', 'Dias'));
+select TRIM upper(concat('Vinicius', ' ', 'Dias')) || ' ');
+
+
+
+
+
